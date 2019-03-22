37b9d6bbd276168096186abe49bb3b92e76a5131
Jason Prasad <jasongprasad@gmail.com>
2019-03-22 10:36:12 -0400

    updating package.json URLs and typography

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,53 +4,29 @@ import { Link } from "gatsby"
 import { rhythm, scale } from "../utils/typography"
 
 class Layout extends React.Component {
-  render() {
-    const { location, title, children } = this.props
-    const rootPath = `${__PATH_PREFIX__}/`
-    let header
+  renderHeader = () =>
+    <h1
+      style={{
+        ...scale(0.75),
+        marginBottom: rhythm(1.5),
+        marginTop: 0,
+      }}
+    >
+      <Link
+        style={{
+          boxShadow: `none`,
+          textDecoration: `none`,
+          color: `inherit`,
+        }}
+        to={`/`}
+      >
+        {this.props.title}
+      </Link>
+    </h1>
 
-    if (location.pathname === rootPath) {
-      header = (
-        <h1
-          style={{
-            ...scale(0.75),
-            marginBottom: rhythm(1.5),
-            marginTop: 0,
-          }}
-        >
-          <Link
-            style={{
-              boxShadow: `none`,
-              textDecoration: `none`,
-              color: `inherit`,
-            }}
-            to={`/`}
-          >
-            {title}
-          </Link>
-        </h1>
-      )
-    } else {
-      header = (
-        <h3
-          style={{
-            fontFamily: `Montserrat, sans-serif`,
-            marginTop: 0,
-          }}
-        >
-          <Link
-            style={{
-              boxShadow: `none`,
-              textDecoration: `none`,
-              color: `inherit`,
-            }}
-            to={`/`}
-          >
-            {title}
-          </Link>
-        </h3>
-      )
-    }
+  render() {
+    const { children } = this.props
+
     return (
       <div
         style={{
@@ -60,7 +36,7 @@ class Layout extends React.Component {
           padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
         }}
       >
-        <header>{header}</header>
+        <header>{this.renderHeader()}</header>
         <main>{children}</main>
       </div>
     )
--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,17 +1,20 @@
 import Typography from "typography"
-import Wordpress2016 from "typography-theme-wordpress-2016"
 
-Wordpress2016.overrideThemeStyles = () => {
-  return {
-    "a.gatsby-resp-image-link": {
-      boxShadow: `none`,
-    },
-  }
-}
+const fonts = [
+  "Avenir Next",
+  "Helvetica Neue",
+  "Segoe UI",
+  "Helvetica",
+  "Arial",
+  "sans-serif",
+]
 
-delete Wordpress2016.googleFonts
-
-const typography = new Typography(Wordpress2016)
+const typography = new Typography({
+  baseFontSize: "18px",
+  baseLineHeight: 1.666,
+  headerFontFamily: fonts,
+  bodyFontFamily: fonts,
+})
 
 // Hot reload typography in development.
 if (process.env.NODE_ENV !== `production`) {
